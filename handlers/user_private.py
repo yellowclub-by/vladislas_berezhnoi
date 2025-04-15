9ab133a3daaa467f3e21602ebd7de811bee305ed
Ilya Chernooki <ilac10765@gmail.com>
2025-04-15 19:01:30 +0300

    lesson 3

---
+++ handlers/user_private.py
@@ -0,0 +1,30 @@
+from aiogram import types, Router, F
+from aiogram.filters import CommandStart, Command
+
+user_router = Router()
+
+@user_router.message(CommandStart())
+async def start(message: types.Message):
+    await (message.answer("Привет, это бот по FunTime валюте!"))
+
+@user_router.message(F.text.lower() == ('каталог'))
+@user_router.message(Command('catalog'))
+async def catalog(message: types.message):
+    await (message.answer('Это каталог'))
+
+@user_router.message(F.text.lower() == ('корзина'))
+@user_router.message(Command('busket'))
+async def busket(message: types.message):
+    await (message.answer('Это корзина'))
+
+@user_router.message(F.text.lower() == ('оплата'))
+@user_router.message(Command('donat'))
+async def donat(message: types.message):
+    await (message.answer('Плати'))
+
+@user_router.message(F.text.lower().contains('стои')|F.text.lower().contains('цен'))
+async def echo(message: types.Message):
+    await(message.answer("Только команды!"))
+    # user_text=message.text
+    # await (message.answer(user_text))
+
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hi world')
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from pyexpat.errors import messages
+
+TOKEN= "7028073659:AAErWTfD_vHqxfOwb823LP7WxcjZg5lL_1A"
+
+bot = Bot(token = TOKEN)
+dp = Dispatcher()
+from handlers.user_private import user_router
+dp.include_router(user_router)
+
+async def main():
+    print("Бот запущен")
+    await dp.start_polling(bot)
+
+asyncio.run(main())
